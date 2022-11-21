8293a4f3af26e2e98f486d0a561b5adec1822458
nyangena1 <gettynyangy@gmail.com>
2022-11-21 12:54:08 +0300

    my commits

---
+++ frontend/src/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+// import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Provider } from "react-redux";
+import store from "./redux/store";
+
+
+import { createRoot } from "react-dom/client";
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+
+ root.render(
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
+  document.getElementById("root")
+);
+
+
+reportWebVitals();
